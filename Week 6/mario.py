24c7e2af25ed97ac708469fbc927bab9c79abbf5
domsterthebot <46486515+domsterthebot@users.noreply.github.com>
2020-06-02 14:49:57 +0800

    Create mario.py

---
+++ Week 6/mario.py
@@ -0,0 +1,28 @@
+def main():
+    isNumber = False
+
+    while not isNumber:
+        try:
+            size = int(input('Height: '))
+
+            if size > 0 and size <= 8:
+                isNumber = True
+                break
+        except ValueError:
+            isNumber = False
+
+    build(size, size)
+
+def build(size, counter):
+    spaces = size - 1
+
+    if size == 0:
+        return 1
+    else:
+        print(' ' * spaces, end='')
+        print('#' * (counter - spaces), end=' ')
+        print('#' * (counter - spaces), end='\n')
+
+        return build(size - 1, counter)
+
+main()
